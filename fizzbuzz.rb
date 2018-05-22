f80403f1446f9eaf280db7ae8b73e41c66a40f16
Sharmaine Requerme <srqrme@gmail.com>
2018-05-22 19:24:55 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,14 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
     return "FizzBuzz"
-  end
-
-  if int % 5 == 0
+  elsif int % 5 == 0
     return "Buzz"
-  end
-
   if int % 3 == 0
     return "Fizz"
   end
-
 end
